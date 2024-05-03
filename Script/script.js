eccd7483ce4270273701c43aaea6e43eb23d9758
r1eda <radamad2016@gmail.com>
2024-05-03 13:12:31 +0300

    تصحيح اخطاء

--- Script/script.js
+++ Script/script.js
@@ -7,41 +7,41 @@ function navigateTo(route) {
 document.addEventListener("DOMContentLoaded", function () {
     // Goals data
     const goals = [
-        {'title': 'No Poverty', 'image': '../../images/no_poverty.jpg'},
-        {'title': 'Zero Hunger', 'image': '../../images/zero_hunger.jpg'},
-        {'title': 'Good Health and Well-being', 'image': '../../images/good_health.jpg'},
-        {'title': 'Quality Education', 'image': '../../images/quality_education.jpg'},
-        {'title': 'Gender Equality', 'image': '../../images/gender_equality.jpg'},
-        {'title': 'Clean Water and Sanitation', 'image': '../../images/clean_water.jpg'},
+        {'title': 'No Poverty', 'image': '../images/no_poverty.jpg'},
+        {'title': 'Zero Hunger', 'image': '../images/zero_hunger.jpg'},
+        {'title': 'Good Health and Well-being', 'image': '../images/good_health.jpg'},
+        {'title': 'Quality Education', 'image': '../images/quality_education.jpg'},
+        {'title': 'Gender Equality', 'image': '../images/gender_equality.jpg'},
+        {'title': 'Clean Water and Sanitation', 'image': '../images/clean_water.jpg'},
         {
           'title': 'Affordable and Clean Energy',
-          'image': '../../images/clean_energy.jpg'
+          'image': '../images/clean_energy.jpg'
         },
         {
           'title': 'Decent Work and Economic Growth',
-          'image': '../../images/economic_growth.jpg'
+          'image': '../images/economic_growth.jpg'
         },
         {
           'title': 'Industry, Innovation, and Infrastructure',
-          'image': '../../images/infrastructure.jpg'
+          'image': '../images/infrastructure.jpg'
         },
-        {'title': 'Reduced Inequality', 'image': '../../images/reduced_inequality.jpg'},
+        {'title': 'Reduced Inequality', 'image': '../images/reduced_inequality.jpg'},
         {
           'title': 'Sustainable Cities and Communities',
-          'image': '../../images/sustainable_cities.jpg'
+          'image': '../images/sustainable_cities.jpg'
         },
         {
           'title': 'Responsible Consumption and Production',
-          'image': '../../images/responsible_consumption.jpg'
+          'image': '../images/responsible_consumption.jpg'
         },
-        {'title': 'Climate Action', 'image': '../../images/climate_action.jpg'},
-        {'title': 'Life Below Water', 'image': '../../images/life_below_water.jpg'},
-        {'title': 'Life on Land', 'image': '../../images/life_on_land.jpg'},
+        {'title': 'Climate Action', 'image': '../images/climate_action.jpg'},
+        {'title': 'Life Below Water', 'image': '../images/life_below_water.jpg'},
+        {'title': 'Life on Land', 'image': '../images/life_on_land.jpg'},
         {
           'title': 'Peace, Justice, and Strong Institutions',
-          'image': '../../images/peace_justice.jpg'
+          'image': '../images/peace_justice.jpg'
         },
-        {'title': 'Partnerships for the Goals', 'image': '../../images/partnerships.jpg'},
+        {'title': 'Partnerships for the Goals', 'image': '../images/partnerships.jpg'},
     ];
 
     // Populate goals grid
